3a614100d397e4b720d1612e962cd3dba5a64433
ThakoorRishwanth <rishijay13@gmail.com>
2024-03-29 18:23:41 +0530

    added Users

--- Frontend/Scripts/admin.js
+++ Frontend/Scripts/admin.js
@@ -6,4 +6,35 @@ toggleButton.onclick = function(){
 }
 
 
-// let TotalData = res.headers.get("X-Total-Count");
+
+let api = "https://zen-mart-2.onrender.com/Grociries";
+
+let TotalData;
+
+let total = document.getElementById("total");
+
+async function fetchData(url){
+    try{
+        let res = await fetch(url);
+        
+        let data = await res.json();
+        
+        TotalData  = data.length;
+        console.log(TotalData);
+        total.textContent = TotalData;
+        
+        
+        
+        // appendData(data);
+    }
+    catch(err){
+        console,log(err);
+    }
+    
+}
+
+
+fetchData(api);
+
+
+
